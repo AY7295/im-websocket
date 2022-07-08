fadef2f272756c2a93167b189fc7a4d52e71b073
AY7295 <Adolph7295@outlook.com>
2022-07-09 00:55:38 +0800

    feat: :bulb: 添加极光消息推送功能

---
+++ model/jpush.go
@@ -0,0 +1,103 @@
+package model
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/spf13/viper"
+	"io"
+	"log"
+	"net/http"
+)
+
+const (
+	titleFormat = "您有一条来自 %s 的新消息"
+	msgFormat   = "内容: %s "
+)
+
+type JPush struct {
+	Platform     []string     `json:"platform"`
+	Audience     Audience     `json:"audience"`
+	Options      Options      `json:"options"`
+	Notification Notification `json:"notification"`
+}
+
+type Audience struct {
+	Alias []string `json:"alias"`
+}
+
+type Options struct {
+	ApnsProduction bool `json:"apns_production"`
+}
+
+type Notification struct {
+	Android Android `json:"android"`
+	Ios     Ios     `json:"ios"`
+}
+
+type Android struct {
+	Title  string            `json:"title"`
+	Alert  string            `json:"alert"`
+	Extras map[string]string `json:"extras"`
+}
+
+type Ios struct {
+	IAlert IAlert            `json:"alert"`
+	Extras map[string]string `json:"extras"`
+}
+
+type IAlert struct {
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+func NewJPush(title, msg string, ids []string, m ...map[string]string) *JPush {
+	j := &JPush{}
+	j.Platform = viper.GetStringSlice("j_push.platform")
+
+	j.Audience.Alias = ids
+
+	j.Options.ApnsProduction = viper.GetBool("j_push.apns_production")
+
+	j.Notification.Android.Title = fmt.Sprintf(titleFormat, title)
+	j.Notification.Android.Alert = fmt.Sprintf(msgFormat, msg)
+	j.Notification.Ios.IAlert.Title = fmt.Sprintf(titleFormat, title)
+	j.Notification.Ios.IAlert.Body = fmt.Sprintf(msgFormat, msg)
+	if len(m) > 0 {
+		j.Notification.Android.Extras = m[0]
+		j.Notification.Ios.Extras = m[0]
+	}
+
+	return j
+}
+
+func (j *JPush) POST() error {
+	body, err := json.Marshal(j)
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", viper.GetString("j_push.url"), bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Authorization", "Basic "+viper.GetString("j_push.token"))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer func(Body io.ReadCloser) {
+		err1 := Body.Close()
+		if err1 != nil {
+			log.Println(err1.Error())
+			return
+		}
+	}(resp.Body)
+
+	return nil
+
+}
--- service/manager.go
+++ service/manager.go
@@ -4,14 +4,12 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"log"
-	"sync"
-	"webSocket-be/service"
+	"webSocket-be/model"
 )
 
 type ClientManager struct {
 	*sync.RWMutex
 	Clients    map[string]*Client
-	Unread     map[string][]DialogMessage
 	Broadcast  chan *Broadcast
 	Reply      chan *Client
 	Register   chan *Client
@@ -33,9 +31,9 @@ func (manager *ClientManager) WS(c *gin.Context) {
 
 	token := c.GetHeader("Authorization")
 
-	client, err := service.VerifyRequest(token, c)
+	client, err := VerifyRequest(token, c)
 	if err != nil {
-		service.ErrorResponse(c, err.Error())
+		ErrorResponse(c, err.Error())
 		return
 	}
 
@@ -50,35 +48,17 @@ func (manager *ClientManager) Start() {
 	for {
 		select {
 		case client := <-manager.Register:
-			//manager.Lock() // 加锁
+
 			manager.Clients[client.User.Id] = client
-			messages := make([]DialogMessage, 0)
-			// tip 发送未读消息
-			for id, unread := range manager.Unread {
-				if id == client.User.Id {
-					for _, msg := range unread {
-						messages = append(messages, msg)
-					}
-					text, err := json.Marshal(messages)
-					if err != nil {
-						log.Println("发送未读消息错误:", err)
-						return
-					}
-					client.Text <- text
-					delete(manager.Unread, id)
-				}
-			}
-			//manager.Unlock() // 解锁
 
 		case client := <-manager.Unregister:
-			//manager.Lock() // 加锁
+
 			_, ok := manager.Clients[client.User.Id]
 
 			if ok {
 				close(client.Text)
 				delete(manager.Clients, client.User.Id)
 			}
-			//manager.Unlock() // 解锁
 
 		case broadcast := <-manager.Broadcast:
 			ToId := broadcast.Message.User.Id
@@ -90,8 +70,7 @@ func (manager *ClientManager) Start() {
 				continue
 			}
 
-			// 在线直接发送()
-			//manager.Lock() // 加锁
+			// 在 当前对话框 直接发送()
 			for id, client := range manager.Clients {
 				if id != ToId {
 					continue
@@ -100,7 +79,6 @@ func (manager *ClientManager) Start() {
 				online = true
 			}
 
-			// 不在线, 先存入未读消息
 			if !online {
 				manager.Unread[ToId] = append(manager.Unread[ToId], broadcast.Message)
 			}
